2a34a2155592d910eb9b531a8ff949e63987a3dc
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2010-08-29 17:17:10 +0200

    added socket.js again

--- test/nodejo.test.js
+++ test/nodejo.test.js
@@ -1,7 +1,18 @@
 var nodejo = require('../lib/nodejo');
 
 module.exports = {
-    'test eval': function(assert) {
-        
-    }
-}
+	'should serve code for websockets': function(assert, beforeExit) {
+    var actual = '';
+    var code = "var sys = require('sys'); setTimeout(function() {sys.print('works');}, 100);";
+    var websocketMockClient = {
+      send : function(code) {
+        actual += code;
+      }
+    };
+    nodejo.serveCodeForWebsocket(code, websocketMockClient);
+    beforeExit(function() {
+  		var expected = '{\"codeStart\":null}\{\"codeChunk\":\"works\"}\{\"codeEnd\":null}';
+      assert.equal(expected, actual);
+    });
+  }
+};
