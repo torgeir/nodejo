9d58019c7fcd019593dde30cd1de44da84254451
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2010-08-29 12:56:44 +0200

    changed to json messaging -> client

--- public/javascripts/nodejo.js
+++ public/javascripts/nodejo.js
@@ -1,47 +1,91 @@
 (function() {
-  var conn;
-  
-  if (window["WebSocket"]) {
-    conn = new WebSocket("ws://localhost:3000/");    
- 
+
+    var nodejo = {
+        init: function(conn) {
+            this.attachListeners(conn);
+        },
+        attachListeners: function() {
+            var that = this;
+            conn.onopen = function() {
+                that.conn = conn;
+            };                                               
+            conn.onclose = function() {
+                that.conn = undefined;
+            };                                               
+            conn.onmessage = function(e) {                 
+                var message = e.data;
+                if (that.onmessage) {
+                    that.onmessage(message);
+                }
+            };
+        },                                  
+        onmessage: function(message) { /* override */ },
+        send: function(message) {
+            if (conn) {
+                conn.send(message);
+            }
+        }
+    };
+
+    var conn;
+    if (window["WebSocket"]) {
+        conn = new WebSocket("ws://localhost:3000/");        
+        nodejo.init(conn);
+    }  
+    $(document).unload(function() {
+         conn.close();
+    });
+                       
+                 
     var code = $('#code');
     var responseDiv = $('#response')[0]; 
     var submitButton = $('#submit');
-  
+                      
     var update = function(text) {
         var previousHtml = responseDiv.innerHTML;
         responseDiv.innerHTML = previousHtml + text;
     };
+                     
     var submit = function() {
-        conn.send(code.val());
+        nodejo.send(code.val());
     };
-    
     submitButton.click(submit);
     code.keyup(function(e) {
         if (event.keyCode == '13' && event.ctrlKey) {
-            e.preventDefault();
             submit();
-        }
-    });
+        }        
+    });         
 
-    conn.onopen = function() {};                                               
-    conn.onclose = function() {};                                               
-    conn.onmessage = function(e) {                 
-        var message = e.data;
-        if (message == 'start') {
+    var actions = {
+        'codeStart': function() {
             responseDiv.innerHTML = '<pre>';
-        }                                   
-        else if (message == 'end') {
-            responseDiv.innerHTML = responseDiv.innerHTML + '</pre>';
-        }                              
-        else {
-            update(message);
+        },
+        'codeChunk': function(chunk) {
+            update(chunk);
+        },
+        'codeEnd': function() {
+            var content = responseDiv.innerHTML;
+            responseDiv.innerHTML = content + '</pre>';
+        },
+        'codeErr': function() {
+            
         }
     };
-  }  
-  
-  $(document).unload(function() {
-     conn.close();
-  });
+    nodejo.onmessage = function(json) { 
+        try {
+            var message = jQuery.parseJSON(json);
+            for(var key in message) {        
+                var data = message[key];
+                var action = actions[key];
+                if (action) { 
+                    action(data);
+                };
+            }                                              
+        } catch(e) {
+            responseDiv.innerHTML = '<pre>Error parsing json from server.</pre>';
+        }
+    };
+      
+    
   
 })();
