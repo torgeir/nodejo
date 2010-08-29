c83d8162259cab558aee25831e8437d903fda758
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2010-08-29 13:08:48 +0200

    gnab in desperate need of softtabs 2

--- app.js
+++ app.js
@@ -15,43 +15,40 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.use(connect.bodyDecoder());
-    app.use(connect.methodOverride());
-    app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
-    app.use(app.router);         
-    app.use(connect.staticProvider(__dirname + '/public'));
-    app.use(connect.staticProvider(__dirname + '/socket.io'));
+  app.set('views', __dirname + '/views');
+  app.use(connect.bodyDecoder());
+  app.use(connect.methodOverride());
+  app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
+  app.use(app.router);         
+  app.use(connect.staticProvider(__dirname + '/public'));
+  app.use(connect.staticProvider(__dirname + '/socket.io'));
 });
 
 app.configure('development', function(){
-    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-   app.use(connect.errorHandler()); 
+  app.use(connect.errorHandler()); 
 });
 
 // Routes           
 app.get('/', function(req, res) {
-    res.render(
-        'index.jade', 
-        {
-            locals: {
-                title: 'Nodejo'
-            }
-        }
-    );
-
+  res.render(
+    'index.jade', 
+    {
+      locals: {
+        title: 'Nodejo'
+      }
+    }
+  );
 });
 
 var socket = ws.createServer({ server: app });
 socket.addListener("listening", function(){});
 socket.addListener('close', function(client) {});
 socket.addListener('connection', function(client) {
-    client.addListener('message', function(message) {
-        nodejo.eval(message, client);
-    });
+  nodejo.handle(client);
 });
 
 // Only listen on $ node app.js
--- public/javascripts/nodejo.js
+++ public/javascripts/nodejo.js
@@ -1,91 +1,89 @@
 (function() {
 
-    var nodejo = {
-        init: function(conn) {
-            this.attachListeners(conn);
-        },
-        attachListeners: function() {
-            var that = this;
-            conn.onopen = function() {
-                that.conn = conn;
-            };                                               
-            conn.onclose = function() {
-                that.conn = undefined;
-            };                                               
-            conn.onmessage = function(e) {                 
-                var message = e.data;
-                if (that.onmessage) {
-                    that.onmessage(message);
-                }
-            };
-        },                                  
-        onmessage: function(message) { /* override */ },
-        send: function(message) {
-            if (conn) {
-                conn.send(message);
-            }
+  var nodejo = {
+    init: function(conn) {
+      this.attachListeners(conn);
+    },
+    attachListeners: function() {
+      var that = this;
+      conn.onopen = function() {
+        that.conn = conn;
+      };                                               
+      conn.onclose = function() {
+        that.conn = undefined;
+      };                                               
+      conn.onmessage = function(e) {                 
+        var message = e.data;
+        if (that.onmessage) {
+          that.onmessage(message);
         }
-    };
+      };
+    },                                  
+    onmessage: function(message) { /* override */ },
+    send: function(message) {
+      if (conn) {
+        conn.send(message);
+      }
+    }
+  };
 
-    var conn;
-    if (window["WebSocket"]) {
-        conn = new WebSocket("ws://localhost:3000/");        
-        nodejo.init(conn);
-    }  
-    $(document).unload(function() {
-         conn.close();
-    });
-                       
-                 
-    var code = $('#code');
-    var responseDiv = $('#response')[0]; 
-    var submitButton = $('#submit');
-                      
-    var update = function(text) {
-        var previousHtml = responseDiv.innerHTML;
-        responseDiv.innerHTML = previousHtml + text;
-    };
+  var conn;
+  if (window["WebSocket"]) {
+    conn = new WebSocket("ws://localhost:3000/");        
+    nodejo.init(conn);
+  }  
+  $(document).unload(function() {
+    conn.close();
+  });
                      
-    var submit = function() {
-        nodejo.send(code.val());
-    };
-    submitButton.click(submit);
-    code.keyup(function(e) {
-        if (event.keyCode == '13' && event.ctrlKey) {
-            submit();
-        }        
-    });         
+               
+  var code = $('#code');
+  var responseDiv = $('#response')[0]; 
+  var submitButton = $('#submit');
+                    
+  var update = function(text) {
+    var previousHtml = responseDiv.innerHTML;
+    responseDiv.innerHTML = previousHtml + text;
+  };
+                   
+  var submit = function() {
+    nodejo.send(code.val());
+  };
+  submitButton.click(submit);
+  code.keyup(function(e) {
+    if (event.keyCode == '13' && event.ctrlKey) {
+      submit();
+    }        
+  });         
 
-    var actions = {
-        'codeStart': function() {
-            responseDiv.innerHTML = '<pre>';
-        },
-        'codeChunk': function(chunk) {
-            update(chunk);
-        },
-        'codeEnd': function() {
-            var content = responseDiv.innerHTML;
-            responseDiv.innerHTML = content + '</pre>';
-        },
-        'codeErr': function() {
-            
-        }
-    };
-    nodejo.onmessage = function(json) { 
-        try {
-            var message = jQuery.parseJSON(json);
-            for(var key in message) {        
-                var data = message[key];
-                var action = actions[key];
-                if (action) { 
-                    action(data);
-                };
-            }                                              
-        } catch(e) {
-            responseDiv.innerHTML = '<pre>Error parsing json from server.</pre>';
-        }
-    };
-      
-    
+  var actions = {
+    'codeStart': function() {
+      responseDiv.innerHTML = '<pre>';
+    },
+    'codeChunk': function(chunk) {
+      update(chunk);
+    },
+    'codeEnd': function() {
+      var content = responseDiv.innerHTML;
+      responseDiv.innerHTML = content + '</pre>';
+    },
+    'codeErr': function() {
+        
+    }
+  };
   
+  nodejo.onmessage = function(json) { 
+    try {
+      var message = jQuery.parseJSON(json);
+      for(var key in message) {        
+        var data = message[key];
+        var action = actions[key];
+        if (action) { 
+          action(data);
+        };
+      }                                              
+    } catch(e) {
+      responseDiv.innerHTML = '<pre>Error parsing json from server.</pre>';
+    }
+  };
 })();
