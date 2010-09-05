7fee654704bc2e80f89fd242deff1c964760a798
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2010-09-05 12:58:50 +0200

    added shared json messagehandler

--- public/javascripts/nodejo.js
+++ public/javascripts/nodejo.js
@@ -20,41 +20,28 @@
     submit = function() {
       socket.send(JSON.stringify({ code: editor.getCode() }));   
     };
-
-    var actions = {
-      'codeStart': function() {
-        responseDiv.innerHTML = '<pre>';
-      },
-      'codeChunk': function(chunk) {
-        responseDiv.innerHTML = responseDiv.innerHTML + chunk;
-      },
-      'codeEnd': function() {
-        responseDiv.innerHTML = responseDiv.innerHTML + '</pre>';
-      },
-      'codeErr': function(err) {
-        responseDiv.innerHTML = '<pre>' + err + '</pre>';
-      },
-      'snippetAdd': function(snippet) {
-        snippetsWidget.add(snippet);
-      },
-      'snippet': function(snippet) {
-        editor.setCode(snippet);
-      }
-    };
  
-    socket.onmessage = function(json) {                  
-      try {
-        var message = $.parseJSON(json);
-        for(var key in message) {        
-          var data = message[key];
-          var action = actions[key];
-          if (action) { 
-            action(data);
-          };
-        }                                              
-      } catch(e) {
-        responseDiv.innerHTML = '<pre>Error parsing json from server.</pre>';
-      }
+    socket.onmessage = function(json) {   
+      messageHandler.handle(json, {
+        'codeStart': function() {
+          responseDiv.innerHTML = '<pre>';
+        },
+        'codeChunk': function(chunk) {
+          responseDiv.innerHTML = responseDiv.innerHTML + chunk;
+        },
+        'codeEnd': function() {
+          responseDiv.innerHTML = responseDiv.innerHTML + '</pre>';
+        },
+        'codeErr': function(err) {
+          responseDiv.innerHTML = '<pre>' + err + '</pre>';
+        },
+        'snippetAdd': function(snippet) {
+          snippetsWidget.add(snippet);
+        },
+        'snippet': function(snippet) {
+          editor.setCode(snippet);
+        }
+      });
     };
     
     window.onhashchange = function() {
